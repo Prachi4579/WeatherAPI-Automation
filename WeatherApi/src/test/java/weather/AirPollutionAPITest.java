cb55c636f15c9a693dcb7fcdc6e0ebe75f2f770d
Prachi4579 <prachi4579@gmail.com>
2024-07-12 14:00:24 +0530

    changes

--- WeatherApi/src/test/java/weather/AirPollutionAPITest.java
+++ WeatherApi/src/test/java/weather/AirPollutionAPITest.java
@@ -6,28 +6,28 @@ import report.ListenertestNG;
 
 @Listeners(ListenertestNG.class)
 public class AirPollutionAPITest extends Parameter {
-//
-//	@Test(testName="Verify AirPollution Endpoint returning 200 OK while sending request with GET method", groups = {"TC-008","smoke", "sanity" })
-//	public void verifyAirPollutionEndpoint() {
-//		setupParams("TC-008"); 
-//		Response resp = request.get(getConfiguration("airpollution_endpoint"));
-//		assertResponse(resp, 200);
-//	}
-//
-//	@Test(testName = "Verify AirPollution Endpoint returning Method not allowed 405 while sending request with PUT method", groups = {
-//			"TC-009", "smoke", "sanity" })
-//	public void verifyPutCallAirPollutionEndpoint() {
-//		setupParams("TC-009");
-//		Response resp = request.put(getConfiguration("airpollution_endpoint"));
-//		assertResponse(resp, 405);
-//	}
 
-	@Test(testName = "Verify AirPollution Post method Data", groups = { "TC-01", "smoke", "sanity" })
-	public void verifyPostCallAirPollutionEndpoint() {
-		setupParams("TC-001");
-		Response resp = request.post(getConfiguration("airpollution_endpoint"));
+	@Test(testName="Verify AirPollution Endpoint returning 200 OK while sending request with GET method", groups = {"TC-008","smoke", "sanity" })
+	public void verifyAirPollutionEndpoint() {
+		setupParams("TC-008"); 
+		Response resp = request.get(getConfiguration("airpollution_endpoint"));
+		assertResponse(resp, 200);
+	}
+
+	@Test(testName = "Verify AirPollution Endpoint returning Method not allowed 405 while sending request with PUT method", groups = {
+			"TC-009", "smoke", "sanity" })
+	public void verifyPutCallAirPollutionEndpoint() {
+		setupParams("TC-009");
+		Response resp = request.put(getConfiguration("airpollution_endpoint"));
 		assertResponse(resp, 405);
 	}
+
+//	@Test(testName = "Verify AirPollution Post method Data", groups = { "TC-01", "smoke", "sanity" })
+//	public void verifyPostCallAirPollutionEndpoint() {
+//		setupParams("TC-001");
+//		Response resp = request.post(getConfiguration("airpollution_endpoint"));
+//		assertResponse(resp, 405);
+//	}
 }
 
 
